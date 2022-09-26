1f35a7b2dfc284ebea2f5b5a6bb6caf8a4eafa4c
Victor Ramos <110069191+oramosvictor@users.noreply.github.com>
2022-09-26 15:48:07 -0300

    Aula 1 - Curso de Python com Prof Claudio Bonel

---
+++ Python/Aula01.py
@@ -0,0 +1,47 @@
+#Primeiro Códico em Python, pra dar sorte!
+print ("Hello World!")
+
+#Variáveis
+#Numero inteiro
+numero_inteiro = 10
+print(numero_inteiro)
+
+#String
+texto = "Alo, fala comigo Filho"
+print(texto)
+
+#Decimal
+numero_decimal = 5.6
+print(numero_decimal)
+
+#Operadores logicos (Boleana)
+# == Igual Ex: 15 == 20 "False"
+# > Maior
+# >= Maior ou Igual
+# Essas acima servem para menor também
+# != Diferente
+
+valor = 15 <= 15
+print(valor)
+
+#Ex1
+dolar = 5.17
+alvo = 5.04
+valida = dolar < alvo
+print(valida)
+
+#Funções e Métodos
+
+#Exemplo de Função
+print ("Função")
+
+
+#Exemplo de Método
+texto = texto.replace("Alo","Alô")
+texto = texto.upper()
+print(texto)
+
+#Para extrair do inicio para o fim do texto use ":", para do fim para o inicio usa "-" e no meio coloque
+#por exempo "5:8" que pega os caracteres neste intervalo
+a = texto [:3]
+print(a)
